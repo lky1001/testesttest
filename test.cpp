615e45de9c90ca718106331ee4a38c00c9227b8a
epochchaser <40057797+epochchaser@users.noreply.github.com>
2019-03-08 14:14:50 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -32,7 +32,7 @@ void love_tattoo::matchcouple(name from, name to)
     eosio_assert(is_account(to), "to account does not exist");
 
     // from 유저의 커플 매칭 내역 찾기
-    couple_index couple_table(_code, from);
+    couple_index couple_table(_code, from.value);
     auto from_user_index = couple_table.get_index<"byfromuser"_n>();
     auto from_user_iterator = from_user_index.find(from.value);
 
@@ -65,71 +65,71 @@ void love_tattoo::matchcouple(name from, name to)
         auto to_user_iterator = to_user_index.find(from.value);
 
         // 매칭 요청이 있을 경우
-        if (to_user_iterator != couple_index.end())
-        {
-            // 내가 매칭 요청 상대와 나를 매칭 요청한 상대가 같을 경우 매칭 완료
-            if (to_user_iterator->from_user.value == to.value)
-            {
-                uint64_t cur_time = current_time();
+//         if (to_user_iterator != couple_index.end())
+//         {
+//             // 내가 매칭 요청 상대와 나를 매칭 요청한 상대가 같을 경우 매칭 완료
+//             if (to_user_iterator->from_user.value == to.value)
+//             {
+//                 uint64_t cur_time = current_time();
 
-                user_index user_table(_code, _code);
-                auto from_user_index = couple_table.get_index<"byowner"_n>();
-                auto from_user_iterator = from_user_index.find(from.value);
+//                 user_index user_table(_code, _code);
+//                 auto from_user_index = couple_table.get_index<"byowner"_n>();
+//                 auto from_user_iterator = from_user_index.find(from.value);
 
-                if (from_user_iterator != user_index.end())
-                {
-                    user_table.modify(from_user_iterator, _code, [&](auto &row) {
-                        row.match_user = to;
-                        row.matching_date = cur_time;
-                        row.updated = cur_time;
-                    });
-                }
-                else
-                {
-                    eosio_assert(false, "from user not found.");
-                }
+//                 if (from_user_iterator != user_index.end())
+//                 {
+//                     user_table.modify(from_user_iterator, _code, [&](auto &row) {
+//                         row.match_user = to;
+//                         row.matching_date = cur_time;
+//                         row.updated = cur_time;
+//                     });
+//                 }
+//                 else
+//                 {
+//                     eosio_assert(false, "from user not found.");
+//                 }
 
-                auto to_user_index = couple_table.get_index<"byowner"_n>();
-                auto to_user_iterator = to_user_index.find(to.value);
+//                 auto to_user_index = couple_table.get_index<"byowner"_n>();
+//                 auto to_user_iterator = to_user_index.find(to.value);
 
-                if (to_user_iterator != user_index.end())
-                {
-                    user_table.modify(to_user_iterator, _code, [&](auto &row) {
-                        row.match_user = from;
-                        row.matching_date = cur_time;
-                        row.updated = cur_time;
-                    });
-                }
-                else
-                {
-                    eosio_assert(false, "to user not found.");
-                }
-            }
-            else
-            {
-                // 그렇지 않을 경우 신규 추가
-                uint64_t cur_time = current_time();
-                // 신규 추가
-                couple_table.emplace(from, [&](auto &row) {
-                    row.from_user = from;
-                    row.to_user = to;
-                    row.expired = cur_time + cur_time + ((uint64_t)(1LLU * 86400 * 1000000));
-                    row.created = cur_time;
-                });
-            }
-        }
-        else
-        {
-            uint64_t cur_time = current_time();
-            // 신규 추가
-            couple_table.emplace(from, [&](auto &row) {
-                row.id = couple_table.available_primary_key();
-                row.from_user = from;
-                row.to_user = to;
-                row.expired = cur_time + cur_time + ((uint64_t)(1LLU * 86400 * 1000000));
-                row.created = cur_time;
-            });
-        }
+//                 if (to_user_iterator != user_index.end())
+//                 {
+//                     user_table.modify(to_user_iterator, _code, [&](auto &row) {
+//                         row.match_user = from;
+//                         row.matching_date = cur_time;
+//                         row.updated = cur_time;
+//                     });
+//                 }
+//                 else
+//                 {
+//                     eosio_assert(false, "to user not found.");
+//                 }
+//             }
+//             else
+//             {
+//                 // 그렇지 않을 경우 신규 추가
+//                 uint64_t cur_time = current_time();
+//                 // 신규 추가
+//                 couple_table.emplace(from, [&](auto &row) {
+//                     row.from_user = from;
+//                     row.to_user = to;
+//                     row.expired = cur_time + cur_time + ((uint64_t)(1LLU * 86400 * 1000000));
+//                     row.created = cur_time;
+//                 });
+//             }
+//         }
+//         else
+//         {
+//             uint64_t cur_time = current_time();
+//             // 신규 추가
+//             couple_table.emplace(from, [&](auto &row) {
+//                 row.id = couple_table.available_primary_key();
+//                 row.from_user = from;
+//                 row.to_user = to;
+//                 row.expired = cur_time + cur_time + ((uint64_t)(1LLU * 86400 * 1000000));
+//                 row.created = cur_time;
+//             });
+//         }
     }
 }
 
